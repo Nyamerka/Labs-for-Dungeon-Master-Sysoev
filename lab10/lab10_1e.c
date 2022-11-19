9855268a388bd780f0e2bc8fe510169afd8683cb
Nyamerka <113332837+Nyamerka@users.noreply.github.com>
2022-11-19 16:38:48 +0300

    Add files via upload

---
+++ lab10/lab10_1e.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main () {
+    int size;
+    scanf("%d", &size);
+    int array [size];
+    for (i = 0, i < size, i++) {
+        scanf("%d", &array [size]);
+    }
+    return 0;
+}
